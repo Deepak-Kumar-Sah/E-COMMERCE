54d4229818a95e1112aa011d433769fc09717cb6
Deepak Kumar <deepaksah9998@gmail.com>
2022-07-23 15:56:16 +0530

    Category Schema added

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,5 +1,7 @@
 //Schema for user
-const mongoose=require("mongoose")
+const mongoose=require("mongoose");
+const crypto = require("crypto");
+const { v4: uuidv4 } = require('uuid');
 
 const Schema =mongoose.Schema;
 
@@ -26,7 +28,7 @@ const userSchema = new Schema({
     trim:true
    },
    //TODO come back here
-   password:{
+   encry_password:{
     type:String,
     required:true,
    },
@@ -39,5 +41,31 @@ const userSchema = new Schema({
     type:Array,
     default:[]
    }
-});
+},{timestamps:true});
+//Virtual field creation
+userSchema.virtual("password")
+   .set(function(password){
+      this._password = password
+      this.salt = uuidv4();
+      this.encry_password =this.securePassword(password)
+   })
+   .get(function(){
+      return this._password
+   })
+
+userSchema.method = {
+   authentication:function(plainpassword){
+      return this.securePassword(plainpassword)==this.encry_password
+   },
+   securePassword:function(plainpassword){
+      if(!password) return "";
+      try {
+         return crypto.createHmac('sha256',this.salt)
+         .update(plainpassword)
+         .digest('hex');
+      } catch (err) {
+         console.log(err.message);
+      }
+   }
+}
 module.exports=mongoose.model("User",userSchema);
