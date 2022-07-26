fc3fa8b703b27e42c64cf92d9dec137171784f22
Deepak Kumar <deepaksah9998@gmail.com>
2022-07-26 11:47:28 +0530

    Custom middleware added

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router()
 const {check,validationResult} = require("express-validator");
 
-const {signout,signup,signin}=require("../controllers/auth");
+const {signout,signup,signin, isSignedIn}=require("../controllers/auth");
 
 router.post("/signup",[
     check("name").isLength({min:3}).withMessage("name should be at least 3 char"),
@@ -16,6 +16,8 @@ router.post("/signin",[
 ],signin);
 
 router.get("/signout",signout);
-
+router.get("/testroute",isSignedIn,(req,res)=>{
+    res.send("A protected route");
+})
 
 module.exports = router;
